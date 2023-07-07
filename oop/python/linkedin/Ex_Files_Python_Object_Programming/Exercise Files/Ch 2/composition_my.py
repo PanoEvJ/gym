1c9b342d9ac63ed27ac05d602b1264fe50449a3a
PanoEvJ <panosevag@gmail.com>
2023-07-07 22:42:19 +0200

    completed composition_my

--- oop/python/linkedin/Ex_Files_Python_Object_Programming/Exercise Files/Ch 2/composition_my.py
+++ oop/python/linkedin/Ex_Files_Python_Object_Programming/Exercise Files/Ch 2/composition_my.py
@@ -1,25 +1,45 @@
 # Using composition to build complex objects
 
 class Book:
-    def __init__(self, title, price, authorfname, authorlname):
+    def __init__(self, title, price, author=None):
         self.title = title
         self.price = price
-        self.authorfname = authorfname
-        self.authorlname = authorlname
+        self.author = author
         
         self.chapters = []
         
-    def addchapter(self, name, pages):
-        self.chapters.append((name,pages))
+    def addchapter(self, chapter):
+        self.chapters.append(chapter)
     
-    # def pagecount(self):
-    #     result = 0
-    #     for ch in self.chapters:
-            
+    def getbookpagecount(self):
+        result = 0
+        for ch in self.chapters:
+            result += ch.pagecount
+        return result
+    
+class Author:
+    def __init__(self, fname, lname):
+        self.fname = fname
+        self.lname = lname
         
+    def __str__(self):
+        return f"{self.fname} {self.lname}"
+    
+class Chapter:
+    def __init__(self, name, pagecount):
+        self.name = name
+        self.pagecount = pagecount
+        
+auth = Author("Tom", "Robbins")
 
-b1 = Book("The Jitterbug Perfume", 29.9, "Tom", "Robbins")
-b1.addchapter("Chapter 1", 24)
-b1.addchapter("Chapter 2", 43)
+b1 = Book("The Jitterbug Perfume", 29.9, auth)
+
+chapter1 = Chapter("Chapter 1", 24)
+chapter2 = Chapter("Chapter 2", 43)
+
+b1.addchapter(chapter1)
+b1.addchapter(chapter2)
 
 print(b1.title)      
+print(b1.author)      
+print(b1.getbookpagecount())      
