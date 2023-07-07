048413d50a470de24bb340cb8ed3fd4391a7400c
PanoEvJ <panosevag@gmail.com>
2023-07-07 15:08:49 +0200

    initiated my_sandbox document

---
+++ oop/python/linkedin/Ex_Files_Python_Object_Programming/Exercise Files/Ch 1/my_sandbox.py
@@ -0,0 +1,7 @@
+# Create a Book class
+# Assign instance variables and instace methods
+# Assign class variables and class methods
+# Assign static variables and static methods
+# Create another class and typecheck
+
+
