73c9167d11ec482c76f3aa532acb1a40f2abbbb7
PanoEvJ <panosevag@gmail.com>
2023-07-07 21:57:39 +0200

    completed interface_my

--- oop/python/linkedin/Ex_Files_Python_Object_Programming/Exercise Files/Ch 2/interface_my.py
+++ oop/python/linkedin/Ex_Files_Python_Object_Programming/Exercise Files/Ch 2/interface_my.py
@@ -1,6 +1,13 @@
 # Using Abstract Base Classes to implement interfaces
 from abc import ABC, abstractmethod
 
+class JSONify(ABC):
+    def __init__(self):
+        super().__init__()
+    
+    @abstractmethod
+    def toJSON(self):
+        pass
 
 class GraphicShape(ABC):
     def __init__(self):
@@ -10,15 +17,19 @@ class GraphicShape(ABC):
     def calcArea(self):
         pass
     
-class Circle(GraphicShape):
+class Circle(GraphicShape, JSONify):
     def __init__(self, radius):
         # super().__init__()
         self.radius = radius
         
     def calcArea(self):
         return 3.14 * (self.radius ** 2)
+    
+    def toJSON(self):
+        return f"{{ \"Circle\" : {str(self.calcArea())} }}"
 
 c = Circle(1.3)
 
 print( 'Cirle area: ', c.calcArea())
+print(c.toJSON())
 
