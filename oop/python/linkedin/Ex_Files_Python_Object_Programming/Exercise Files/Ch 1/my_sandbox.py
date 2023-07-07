07860f0200d42253384156ba146dbbed0d39eff9
PanoEvJ <panosevag@gmail.com>
2023-07-07 15:19:13 +0200

    created & printed instance variables

--- oop/python/linkedin/Ex_Files_Python_Object_Programming/Exercise Files/Ch 1/my_sandbox.py
+++ oop/python/linkedin/Ex_Files_Python_Object_Programming/Exercise Files/Ch 1/my_sandbox.py
@@ -6,11 +6,17 @@
 
 class Book:
     
-    def __init__(self):
-        pass
+    def __init__(self, title, price, pages):
+        self.title = title
+        self.price = price
+        self.pages = pages
 
 print(Book)
 
-b1 = Book()
+b1 = Book("The Jitterbug Perfume", 23, 304)
+print(b1)
 print(type(b1))
 print(isinstance(b1, Book))
+
+print("Book: \n title {}, \n price{}, \n pages{}".format(b1.title, b1.price, b1.pages))
+print(f"Book: \n title {b1.title}, \n price{b1.price}, \n pages{b1.pages}")
