e9be67fd3e80626e6d915a2ee9f738908ebf2265
PanoEvJ <panosevag@gmail.com>
2023-07-07 16:12:39 +0200

    added instance method

--- oop/python/linkedin/Ex_Files_Python_Object_Programming/Exercise Files/Ch 1/my_sandbox.py
+++ oop/python/linkedin/Ex_Files_Python_Object_Programming/Exercise Files/Ch 1/my_sandbox.py
@@ -10,13 +10,23 @@ class Book:
         self.title = title
         self.price = price
         self.pages = pages
+        
+    def getprice(self):
+        return self.price
 
 print(Book)
 
 b1 = Book("The Jitterbug Perfume", 23, 304)
+
 print(b1)
 print(type(b1))
 print(isinstance(b1, Book))
 
 print("Book: \n title {}, \n price{}, \n pages{}".format(b1.title, b1.price, b1.pages))
 print(f"Book: \n title {b1.title}, \n price{b1.price}, \n pages{b1.pages}")
+
+print(b1.getprice())
+
+prc1 = b1.getprice
+print(type(prc1))
+
