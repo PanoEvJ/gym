7fe104bd436bc30f8009ef23cadc005e04dfbe31
PanoEvJ <panosevag@gmail.com>
2023-07-08 19:33:12 +0200

    setup __str__

--- oop/python/linkedin/Ex_Files_Python_Object_Programming/Exercise Files/Ch 3/magicatrr_my.py
+++ oop/python/linkedin/Ex_Files_Python_Object_Programming/Exercise Files/Ch 3/magicatrr_my.py
@@ -7,6 +7,20 @@ class Book:
         self.price = price   
         self._discount = 0.1  
 
+    # The __str__ function is used to return a user-friendly string
+    # representation of the object
+    def __str__(self) -> str:
+        return f"book {self.title} by {self.author}, cost {self.price}"
+
+    # TODO: __getattribute__ called when an attr is retrieved. Don't
+    # directly access the attr name otherwise a recursive loop is created
+
+    # TODO: __setattr__ called when an attribute value is set. Don't set the attr
+    # directly here otherwise a recursive loop causes a crash
+
+    # TODO: __getattr__ called when __getattribute__ lookup fails - you can
+    # pretty much generate attributes on the fly with this method
+    
 b1 = Book("War and Peace", "Leo Tolstoy", 39.95)
 b2 = Book("The Catcher in the Rye", "JD Salinger", 29.95)
 
