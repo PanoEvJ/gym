4de424dab1549a63c1b4cb2a89ef1fa8753866b4
PanoEvJ <panosevag@gmail.com>
2023-07-08 19:30:21 +0200

    basic setup

--- oop/python/linkedin/Ex_Files_Python_Object_Programming/Exercise Files/Ch 3/magicatrr_my.py
+++ oop/python/linkedin/Ex_Files_Python_Object_Programming/Exercise Files/Ch 3/magicatrr_my.py
@@ -1,3 +1,13 @@
 # Using the __getattribute__ and __setattribute methods
 
-# class Book:
+class Book:
+    def __init__(self, title, author, price) -> None:
+        self.title = title
+        self.author = author
+        self.price = price   
+        self._discount = 0.1  
+
+b1 = Book("War and Peace", "Leo Tolstoy", 39.95)
+b2 = Book("The Catcher in the Rye", "JD Salinger", 29.95)
+
+print(b1.price)
