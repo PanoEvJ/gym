5337341ddf6d4faa228e7a7dc937b54d755ad4ef
PanoEvJ <panosevag@gmail.com>
2023-07-10 21:06:09 +0200

    completed magicatrr_my

--- oop/python/linkedin/Ex_Files_Python_Object_Programming/Exercise Files/Ch 3/magicatrr_my.py
+++ oop/python/linkedin/Ex_Files_Python_Object_Programming/Exercise Files/Ch 3/magicatrr_my.py
@@ -26,12 +26,22 @@ class Book:
 
     # TODO: __setattr__ called when an attribute value is set. Don't set the attr
     # directly here otherwise a recursive loop causes a crash
+    def __setattr__(self, name: str, value: Any) -> None:
+        if (name == 'price'):
+            if not isinstance(value, float):
+                raise ValueError(f"{name} is not float")
+        return super().__setattr__(name, value)
 
     # TODO: __getattr__ called when __getattribute__ lookup fails - you can
     # pretty much generate attributes on the fly with this method
-    
+    def __getattr__(self, name):
+        return name + " is not here!"
+    # 
+        # return name + " is not here!"
 b1 = Book("War and Peace", "Leo Tolstoy", 39.95)
 b2 = Book("The Catcher in the Rye", "JD Salinger", 29.95)
 
 print(b1)
+b1.price = 35.95
 print(b1.price)
+print(b1.randomprop)
