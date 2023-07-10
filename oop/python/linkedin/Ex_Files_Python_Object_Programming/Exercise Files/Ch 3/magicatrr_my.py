e3e49b1b8744de59a6e325f26c98e08305e855f0
PanoEvJ <panosevag@gmail.com>
2023-07-10 17:28:26 +0200

    restart magicattr_my.py

--- oop/python/linkedin/Ex_Files_Python_Object_Programming/Exercise Files/Ch 3/magicatrr_my.py
+++ oop/python/linkedin/Ex_Files_Python_Object_Programming/Exercise Files/Ch 3/magicatrr_my.py
@@ -1,10 +1,13 @@
 # Using the __getattribute__ and __setattribute methods
 
+from typing import Any
+
+
 class Book:
     def __init__(self, title, author, price) -> None:
-        self.title = title
-        self.author = author
-        self.price = price   
+        self.title     = title
+        self.author    = author
+        self.price     = price   
         self._discount = 0.1  
 
     # The __str__ function is used to return a user-friendly string
@@ -14,6 +17,12 @@ class Book:
 
     # TODO: __getattribute__ called when an attr is retrieved. Don't
     # directly access the attr name otherwise a recursive loop is created
+    def __getattribute__(self, name: str) -> Any:
+        if (name == "price"):
+            p = super().__getattribute__("price")
+            d = super().__getattribute__("_discount")
+            return p * (1 - d)
+        return super().__getattribute__(name)
 
     # TODO: __setattr__ called when an attribute value is set. Don't set the attr
     # directly here otherwise a recursive loop causes a crash
@@ -24,4 +33,5 @@ class Book:
 b1 = Book("War and Peace", "Leo Tolstoy", 39.95)
 b2 = Book("The Catcher in the Rye", "JD Salinger", 29.95)
 
+print(b1)
 print(b1.price)
