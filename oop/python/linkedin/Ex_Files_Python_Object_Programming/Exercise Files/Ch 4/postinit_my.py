0ad092a5c27af2b5fd50ea6c2ea41f8f5d13ad32
PanoEvJ <panosevag@gmail.com>
2023-07-10 23:18:29 +0200

    completed postinit_my.py

--- oop/python/linkedin/Ex_Files_Python_Object_Programming/Exercise Files/Ch 4/postinit_my.py
+++ oop/python/linkedin/Ex_Files_Python_Object_Programming/Exercise Files/Ch 4/postinit_my.py
@@ -1,7 +1,6 @@
-# Using the postinit function in data classes
+# Using the __post_init__ function in data classes
 
 from dataclasses import dataclass
-
 
 @dataclass
 class Book:
@@ -12,10 +11,12 @@ class Book:
 
     # TODO: the __post_init__ function lets us customize additional properties
     # after the object has been initialized via built-in __init__
-
+    def __post_init__(self):
+        self.description = f"{self.title} by {self.author}"
 
 # create some Book objects
 b1 = Book("War and Peace", "Leo Tolstoy", 1225, 39.95)
 b2 = Book("The Catcher in the Rye", "JD Salinger", 234, 29.95)
 
 # TODO: use the description attribute
+print(b1.description)
