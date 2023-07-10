2c87c5c372e43fdf81a3629b7442ceb51bbc84fc
PanoEvJ <panosevag@gmail.com>
2023-07-10 22:55:14 +0200

    completed dataclass_my.py

--- oop/python/linkedin/Ex_Files_Python_Object_Programming/Exercise Files/Ch 4/daraclass_my.py
+++ oop/python/linkedin/Ex_Files_Python_Object_Programming/Exercise Files/Ch 4/daraclass_my.py
@@ -2,3 +2,40 @@
 
 from dataclasses import dataclass
 
+@dataclass
+class Book:
+    title : str
+    author : str
+    pages : int
+    price : int
+    
+    def bookinfo(self):
+        return f"{self.title} by {self.author}"
+    
+    # def __str__(self):
+    #     return f"{self.title} by {self.author} costs {self.title}"
+    
+    # def __call__(self, title):
+    #     self.title = title
+    #     return "Set favorite book"
+    
+    # def getfavoritebook(self):
+    #     return f"My favorite book is {self.title}"
+    
+# create some instances
+b1 = Book("War and Peace", "Leo Tolstoy", 1225, 39.95)
+b2 = Book("The Catcher in the Rye", "JD Salinger", 234, 29.95)
+b3 = Book("The Catcher in the Rye", "JD Salinger", 234, 29.95)
+
+print(b2)
+print(b1.title)
+# print(b1.bookinfo())
+# print(b1("The Jitterbug Perfume"))
+# print(b1.getfavoritebook())
+
+# comparing two dataclasses
+print(b2 == b3)
+
+# change some fields
+b1.title = 'The Master and Margarita'
+print(b1.bookinfo())
