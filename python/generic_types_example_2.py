660dd617a0e8668e61aa09663c46196f69475b22
Pano <panosevag@gmail.com>
2024-01-02 20:56:42 +0200

    initialized new example

---
+++ python/generic_types_example_2.py
@@ -0,0 +1,21 @@
+from typing import TypeVar
+
+T = TypeVar("T")
+
+
+def process_elements(elements: list[T]) -> list[T]:
+    return [element for index, element in enumerate(elements) if index % 2 == 1]
+
+
+def main():
+    my_list = [1, 2, 3, 4, 5]
+    processed = process_elements(elements=my_list)
+    print(processed)
+
+    my_str_list = ["a", "b", "c", "d", "e"]
+    processed = process_elements(elements=my_str_list)
+    print(processed)
+
+
+if __name__ == "__main__":
+    main()
