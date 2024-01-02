7099c5dcf9d79bb72a31c0ac44f76a62c18448a4
Pano <panosevag@gmail.com>
2024-01-02 21:08:42 +0200

    introduced example with integers for generic func

--- python/generic_types_example_2.py
+++ python/generic_types_example_2.py
@@ -20,7 +20,7 @@ class Box(Generic[T]):
         self.item = item
 
 
-# generic function example
+# generic function
 def get_first_item(items: list[T]) -> T:
     return items[0]
 
@@ -37,6 +37,10 @@ def main():
     # for lists
     list_box = Box(item=[1, 2, 3])
     print(list_box.get_item())
+
+    # generic function example
+    first_item = get_first_item(items=[1, 2, 3])
+    print(first_item)
 
 
 if __name__ == "__main__":
