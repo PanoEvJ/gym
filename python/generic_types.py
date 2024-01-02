60b448ec896ef8626fc3997241a47c81a8ef9e5a
Pano <panosevag@gmail.com>
2024-01-02 20:39:37 +0200

    removed process_numbers

--- python/generic_types.py
+++ python/generic_types.py
@@ -3,10 +3,6 @@ from typing import TypeVar
 T = TypeVar("T")
 
 
-def process_numbers(numbers: list[int]) -> list[int]:
-    return [number + 1 for number in numbers]
-
-
 def process_elements(elements: list[T]) -> list[T]:
     return [element for index, element in enumerate(elements) if index % 2 == 1]
 
